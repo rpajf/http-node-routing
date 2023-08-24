ac68fe3d31e33d649b2161e5a148448cab018ab4
raphaelportela <rafaeljansenaraujo@gmail.com>
2023-08-24 14:22:50 -0300

    regenerate dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,3 @@
-import { createNodeRouter } from './nodeRouter.js';
-import { databaseFunctions } from './connection/index';
+import { createNodeRouter } from './nodeRouter';
+import { databaseFunctions } from './connection';
 export { createNodeRouter, databaseFunctions };
